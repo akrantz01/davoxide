cb3f40057fe83f458d14e10c2d6aaf4f51702eb1
Alex Krantz <alex@krantz.dev>
2022-07-21 23:06:04 -0700

    Add request logging

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ use axum::{
     Extension, Router, Server,
 };
 use dav_server::{body::Body as DavBody, localfs::LocalFs, memls::MemLs, DavHandler};
+use tracing::warn;
 
 mod authentication;
+mod logging;
 
 #[tokio::main]
 async fn main() {
+    if dotenv::dotenv().is_err() {
+        warn!(".env file not found");
+    }
+    tracing_subscriber::fmt::init();
+
     let webdav = DavHandler::builder()
         .strip_prefix("/dav")
         .filesystem(LocalFs::new("./files", false, false, false))
@@ -20,7 +27,8 @@ async fn main() {
     let app = Router::new()
         .route("/dav/*path", any(webdav_handler))
         .layer(Extension(webdav))
-        .layer(middleware::from_fn(authentication::middleware));
+        .layer(middleware::from_fn(authentication::middleware))
+        .layer(logging::layer());
 
     Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
